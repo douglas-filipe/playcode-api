84808539453a79a51aeebb12dcfe7d848fecf5de
Estevan T. D. Martins <estevandias7@gmail.com>
2022-02-15 16:05:33 -0300

    feat: mvp initial channel controllers;

--- src/controllers/channel.controller.ts
+++ src/controllers/channel.controller.ts
@@ -17,9 +17,70 @@ export default class ChannelController {
 
       // await userService.update(user);
 
-      return res.json(newChannel);
+      return res.status(201).json(newChannel);
     } catch (e: any) {
       return res.status(400).json({ message: e.message });
+    }
+  }
+
+  static async byId(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const channelService = new ChannelService();
+      const channel = await channelService.byId(id);
+
+      if (!channel) {
+        throw new ResponseError("Channel not found.", 400);
+      }
+
+      return res.json(channel);
+    } catch (e: any) {
+      if (e.message === "Channel not found.") {
+        return res.status(e.statusCode).json({ message: e.message });
+      }
+
+      const message = e.message;
+      return res.status(400).json({ message });
+    }
+  }
+
+  static async update(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { body } = req;
+
+      const channelService = new ChannelService();
+      const channel = await channelService.update(id, body);
+
+      if (!channel) {
+        throw new ResponseError("Channel not found.", 400);
+      }
+
+      return res.json({ message: "channel updated", channel: channel });
+    } catch (e: any) {
+      if (e.message === "Channel not found.") {
+        return res.status(e.statusCode).json({ message: e.message });
+      }
+
+      const message = e.message;
+
+      return res.status(400).json({ message });
+    }
+  }
+
+  static async delete(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+
+      const channelService = new ChannelService();
+      const response = await channelService.delete(id);
+
+      return res.json({ message: response });
+    } catch (e: any) {
+      const message = e.message;
+
+      return res.status(400).json({ message });
     }
   }
 }
