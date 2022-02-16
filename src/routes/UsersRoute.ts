7524e8c11c0d25972035db5a941380b9d5d4adff
Douglas Filipe <douglasfelipe.net77@gmail.com>
2022-02-16 11:14:10 -0400

    fix: routes and controllers using statit method

--- src/routes/UsersRoute.ts
+++ src/routes/UsersRoute.ts
@@ -1,15 +1,18 @@
 import { Router, Express } from "express";
 import { UsersControllers } from "../controllers";
-import { validation, validationUser } from "../middlewares";
+import { validation } from "../middlewares";
 import { createUserModel, loginUserModel } from "../models";
-const usersControllers = new UsersControllers();
 
 const router = Router();
 
 const usersRoute = (app: Express) => {
-  router.post("/", validation(createUserModel), usersControllers.CreateUser);
-  router.post("/login",validationUser(loginUserModel),usersControllers.LoginUser)
-  app.use("/users", router);
+  router.post(
+    "/users",
+    validation(createUserModel),
+    UsersControllers.CreateUser
+  );
+  router.post("/login", validation(loginUserModel), UsersControllers.LoginUser);
+  app.use("/", router);
 };
 
 export default usersRoute;
