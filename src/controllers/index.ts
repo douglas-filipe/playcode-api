2e5c47293f3f5062b66803fd4e5cb8418f06b906
Estevan T. D. Martins <estevandias7@gmail.com>
2022-02-16 16:10:12 -0300

    fix: UserModel import

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,4 @@
 import ChannelController from "./channel.controller";
-import UsersControllers from "./UsersControllers";
+import { UsersControllers } from "./UsersControllers";
 
 export { UsersControllers, ChannelController };
